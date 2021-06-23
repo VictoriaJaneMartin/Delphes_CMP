e6b70bf48cd5655496cc921840c11e7949b2b29a
joshdijkstra <74965856+joshdijkstra@users.noreply.github.com>
2021-06-23 11:45:19 +0100

    Add files via upload

---
+++ DM/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Fri 26 Feb 2021 22:12:07
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
